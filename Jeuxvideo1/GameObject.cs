56dd2dde95204a83b119f142b22eef8dc767d6b6
Philip05 <drouin.philip@carrefour.cegepvicto.ca>
2016-11-08 16:52:23 -0500

    Premier envoie à Github
    Premier jeu vidéo.

---
+++ Jeuxvideo1/GameObject.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jeuxvideo1
+{
+    class GameObject
+    {
+        public bool estvivant; // si le personnage est vivant (condition).
+        public Texture2D sprite; //C'est une photo.
+        public Vector2 position;
+        public Vector2 vitesse;
+    }
+}
