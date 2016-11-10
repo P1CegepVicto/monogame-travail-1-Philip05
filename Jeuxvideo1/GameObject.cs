8e0b0159a940589a915f8bc10e8fbc35f28c4fb5
Philip05 <drouin.philip@carrefour.cegepvicto.ca>
2016-11-10 18:43:08 -0500

    Ajout projectile mais marche pas

--- Jeuxvideo1/GameObject.cs
+++ Jeuxvideo1/GameObject.cs
@@ -10,9 +10,23 @@ namespace Jeuxvideo1
 {
     class GameObject
     {
+        
         public bool estvivant; // si le personnage est vivant (condition).
         public Texture2D sprite; //C'est une photo.
         public Vector2 position;
         public Vector2 vitesse;
+        public Random de1 = new Random();
+        public int positionennemi;
+        public Rectangle RectCollision = new Rectangle();
+
+        public Rectangle GetRect()
+        {
+            RectCollision.X = (int)this.position.X;
+            RectCollision.Y = (int)this.position.Y;
+            RectCollision.Width = this.sprite.Width;
+            RectCollision.Height = this.sprite.Height;
+
+            return RectCollision;
+        }
     }
 }
