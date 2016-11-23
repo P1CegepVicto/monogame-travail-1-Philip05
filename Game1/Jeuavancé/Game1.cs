82884645dd70fbd874fedeb6d6629c24f8a0c80a
Philip05 <drouin.philip@carrefour.cegepvicto.ca>
2016-11-23 16:49:03 -0500

    Version finale du jeu
    Jeu terminé.

--- Game1/Jeuavancé/Game1.cs
+++ Game1/Jeuavancé/Game1.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Framework.Input;
 using System;
+
 
 namespace Jeuavancé
 {
@@ -13,19 +16,32 @@ namespace Jeuavancé
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Gameobject Hero;
-        Gameobject ExplosionEnnemi;
-        Gameobject Ennemi;
+        Gameobject ExplosionEnnemi;       
         Rectangle fenêtre;
-        Gameobject projectilehero;
         Gameobject laser;
         Texture2D Background;
         Random de1 = new Random();
         Gameobject[] tableauennemi;
         Gameobject[] projectilehéro;
-        
+        SoundEffect son1;
+        SoundEffectInstance bombe;
+        SoundEffect son;
+        SoundEffectInstance Sonlaser;
+        Gameobject explosionhero;
+        SpriteFont font;
+        Song spacemusic;
+        int compteurexplohero=0;
+      
 
-        
+
+
+        int compteur=0;
+        int cptennemi = 0;     
+        int v = 0;
        
+
+
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -40,7 +56,7 @@ namespace Jeuavancé
         /// </summary>
         protected override void Initialize()
         {
-           
+
             this.graphics.PreferredBackBufferWidth = graphics.GraphicsDevice.DisplayMode.Width;
             this.graphics.PreferredBackBufferHeight = graphics.GraphicsDevice.DisplayMode.Height;
             this.graphics.ToggleFullScreen(); //ou this.apply.graphicschanges.
@@ -57,11 +73,6 @@ namespace Jeuavancé
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            //Ennemi = new Gameobject();
-            //Ennemi.estvivant = true;
-            //Ennemi.position.X = 1350;
-            //Ennemi.position.Y = 0;
-
             laser = new Gameobject();
             laser.estvivant = true;
 
@@ -70,38 +81,63 @@ namespace Jeuavancé
             Hero.position.X = 30;
             Hero.position.Y = 950;
 
-            projectilehero = new Gameobject();
-            projectilehero.estvivant = true;
+            projectilehéro = new Gameobject[10];
+            for (int y = 0; y < projectilehéro.Length; y++)
+            {
+                projectilehéro[y] = new Gameobject();
+                projectilehéro[y].estvivant = true;
+                projectilehéro[y].position = Hero.position;
+            }
 
 
-            tableauennemi = new Gameobject[8];
-            for(int i =0; i < tableauennemi.Length; i++)
+            tableauennemi = new Gameobject[10];
+            for (int i = 0; i < tableauennemi.Length; i++)
             {
                 tableauennemi[i] = new Gameobject();
-                tableauennemi[i].direction.X = de1.Next(5,15);
+                tableauennemi[i].direction.X = de1.Next(5, 15);
                 tableauennemi[i].direction.Y = de1.Next(1, 8);
                 tableauennemi[i].position.Y = 0;
-                tableauennemi[i].position.X = 1300;             
+                tableauennemi[i].position.X = 1300;
                 tableauennemi[i].estvivant = true;
             }
 
             ExplosionEnnemi = new Gameobject();
             ExplosionEnnemi.estvivant = true;
+            explosionhero = new Gameobject();
+            explosionhero.estvivant = false;
 
 
 
             #region Load fichiers
 
-            for(int g =0; g<tableauennemi.Length;g++)
+            for (int g = 0; g < tableauennemi.Length; g++)
             {
-             tableauennemi[g].sprite = Content.Load<Texture2D>("Ennemi5.png");
+                tableauennemi[g].sprite = Content.Load<Texture2D>("Ennemi5.png");
             }
-           
+
             laser.sprite = Content.Load<Texture2D>("Laser.png");
             Hero.sprite = Content.Load<Texture2D>("Hero2.png");
-            projectilehero.sprite = Content.Load<Texture2D>("Projectilehero2.png");
+
+            for (int l = 0; l < projectilehéro.Length; l++)
+            {
+                projectilehéro[l].sprite = Content.Load<Texture2D>("Laser.png");
+            }
+
             Background = Content.Load<Texture2D>("Background.jpg");
             ExplosionEnnemi.sprite = Content.Load<Texture2D>("explosion2.png");
+            explosionhero.sprite = Content.Load<Texture2D>("explosion3.png");
+            //sons
+            son = Content.Load<SoundEffect>("Sounds\\sonlaser");
+            Sonlaser = son.CreateInstance();
+            son1 = Content.Load<SoundEffect>("Sounds\\Bombe");
+            bombe = son1.CreateInstance();
+           Song spacemusic  = Content.Load<Song>("Sounds\\Spacemusic");
+            MediaPlayer.Play(spacemusic);
+            
+
+            //Spritefont
+
+            font = Content.Load<SpriteFont>("font");
             #endregion
 
         }
@@ -119,7 +155,8 @@ namespace Jeuavancé
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-                                 
+          
+
             #region lancer laser ennemi
             //for (int j = 0; j < tableauennemi.Length; j++)
             //{
@@ -137,6 +174,8 @@ namespace Jeuavancé
 
             #endregion
             #region Déplacements Hero
+           
+           
 
             if (Hero.estvivant == true)
             {
@@ -159,9 +198,9 @@ namespace Jeuavancé
             }
             #endregion
             #region Surface ecran hero
-            if (Hero.estvivant ==true)
+            if (Hero.estvivant == true)
             {
-                if(Hero.position.X <= fenêtre.Left)
+                if (Hero.position.X <= fenêtre.Left)
                 {
                     Hero.position.X = fenêtre.Left;
                 }
@@ -171,7 +210,7 @@ namespace Jeuavancé
                 }
                 if (Hero.position.Y > fenêtre.Bottom - Hero.sprite.Width)
                 {
-                    Hero.position.Y = fenêtre.Bottom- Hero.sprite.Width;
+                    Hero.position.Y = fenêtre.Bottom - Hero.sprite.Width;
                 }
                 if (Hero.position.Y < fenêtre.Top - Hero.sprite.Width)
                 {
@@ -181,76 +220,118 @@ namespace Jeuavancé
             #endregion
             #region Si appuie space Hero lance missile
             if (Hero.estvivant == true)
-            {
+            {    compteur++;
                 if (Keyboard.GetState().IsKeyDown(Keys.Space))
-                {
-                    if(projectilehero.estvivant ==true)
+                 {                  
+                    
+                    if (v < projectilehéro.Length )
                     {
-                        projectilehero.position = Hero.position;
-                        projectilehero.vitesse.X = 40;
-                        projectilehero.estvivant = false;
+                        if (projectilehéro[v].estvivant == true && compteur­ >=10)
+                        {
+                            projectilehéro[v].position = Hero.position;
+                            projectilehéro[v].vitesse.X = 40;
+                            projectilehéro[v].estvivant = false;
+                            Sonlaser.Play();
+                            compteur = 0;
+                            v++;
+                        }                                             
                     }
+                    
 
-                }
-                if(projectilehero.position.X > fenêtre.Right)
-                {
-                    projectilehero.position = Hero.position;
-                    projectilehero.estvivant = true;
+                    else
+                    {
+                        v = 0;
+
+                        for (int y = 0; y < projectilehéro.Length; y++)
+                        {
+                            projectilehéro[y].estvivant = true;                           
+                        }
+                    }                   
+
                 }
             }
-            #endregion
-
             
-            for (int n =0; n<tableauennemi.Length;n++)
-            {
-                if (tableauennemi[n].position.Y > fenêtre.Bottom)
-                {
-                    tableauennemi[n].direction.X = de1.Next(1, 10);
-                    tableauennemi[n].direction.Y = de1.Next(1, 8);
-                    tableauennemi[n].position.X = de1.Next(1000, 1700);
-                    tableauennemi[n].position.Y = 0;
-                    tableauennemi[n].estvivant = true;
-                }
+            #endregion
+         
 
+            for (int n = 0; n < tableauennemi.Length; n++)
+            {
+                
+                    if (tableauennemi[n].position.Y > fenêtre.Bottom)
+                    {
+                        tableauennemi[n].direction.X = de1.Next(1, 10);
+                        tableauennemi[n].direction.Y = de1.Next(1, 8);
+                        tableauennemi[n].position.X = de1.Next(100, 1700);
+                        tableauennemi[n].position.Y = 0;
+                        tableauennemi[n].estvivant = true;
+                                          
+                }
             }
 
-            UpdateHero();
+          
             UpdateEnnemi();
             UpdateProjectileEnnemi();
-           
+            #region Si hero mort reprend position initiale;
+            if (Hero.estvivant == false)
+            {
+                if (compteurexplohero == 0)
+                {
+                    explosionhero.estvivant = true;
+                    explosionhero.position = Hero.position;
+                    compteurexplohero++;
+                }
 
+
+                //Hero.estvivant = true;
+            }
+
+            #endregion
 
             // TODO: Add your update logic here
 
             base.Update(gameTime);
         }
 
-       public void UpdateHero()
+        
+           public void UpdateEnnemi()
         {
-            if(Hero.GetRect().Intersects(laser.GetRect()))
-            {
-                Hero.estvivant = false;
-            }
-        }
-        public void UpdateEnnemi()
-        {
-            for (int a = 0; a < tableauennemi.Length; a++)
-            {
-                tableauennemi[a].position.X += tableauennemi[a].direction.X;
-                tableauennemi[a].position.Y += tableauennemi[a].direction.Y;
-                tableauennemi[a].vitesse.Y = 5;
-                tableauennemi[a].vitesse.X = -10;
 
-
-                if (tableauennemi[a].GetRect().Intersects(projectilehero.GetRect()))
+            for(int j =0; j<tableauennemi.Length; j++)
+            {
+                if(tableauennemi[j].position.Y >fenêtre.Bottom)
                 {
-                    tableauennemi[a].estvivant = false;
-                    ExplosionEnnemi.position = tableauennemi[a].position;
-                    ExplosionEnnemi.vitesse.X = 2;
-                    ExplosionEnnemi.vitesse.Y = 3;
+                    tableauennemi[j].estvivant = true;
                 }
             }
+          
+                for (int a = 0; a < tableauennemi.Length; a++)
+                {
+                if (cptennemi == 5)
+                {
+
+                    tableauennemi[a].position.X += tableauennemi[a].direction.X;
+                    tableauennemi[a].position.Y += tableauennemi[a].direction.Y;
+                    tableauennemi[a].vitesse.Y = 5;
+                    tableauennemi[a].vitesse.X = -10;
+                    cptennemi = 0;
+
+                }
+                    cptennemi++;
+                for (int b = 0; b < projectilehéro.Length; b++)
+                {
+                    if (tableauennemi[a].GetRect().Intersects(projectilehéro[b].GetRect()))
+                    {
+                        tableauennemi[a].estvivant = false;
+                        ExplosionEnnemi.position = tableauennemi[a].position;
+                        ExplosionEnnemi.vitesse.X = 2;
+                        ExplosionEnnemi.vitesse.Y = 3;
+                    }
+                }                   
+            }               
         }
+           
+            
+    
         public void UpdateProjectileEnnemi()
         {
             for (int l = 0; l < tableauennemi.Length; l++)
@@ -258,11 +339,17 @@ namespace Jeuavancé
                 if (Hero.GetRect().Intersects(tableauennemi[l].GetRect()))
                 {
                     Hero.estvivant = false;
+                    explosionhero.vitesse.X = 4;
+                    explosionhero.vitesse.Y = 4;
+                    explosionhero.estvivant = true;
+                    bombe.Play();
                 }
             }
         }
+
         
-       
+
+
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         /// 
         protected override void Draw(GameTime gameTime)
@@ -271,43 +358,66 @@ namespace Jeuavancé
             spriteBatch.Begin();
 
             spriteBatch.Draw(Background, fenêtre, Color.White);
-             #region Dessin ennemi si vivant + laser ennemi vivant            
-            for(int b =0; b< tableauennemi.Length; b++)
+            #region Dessin ennemi si vivant + laser ennemi vivant            
+            for (int b = 0; b < tableauennemi.Length; b++)
             {
                 if (tableauennemi[b].estvivant == true)
                 {
-                    spriteBatch.Draw(tableauennemi[b].sprite, tableauennemi[b].position+=tableauennemi[b].vitesse, Color.White);
-                    
+                   
+                        spriteBatch.Draw(tableauennemi[b].sprite, tableauennemi[b].position += tableauennemi[b].vitesse, Color.White);
                     //if (laser.estvivant == false)
                     //{
                     //    spriteBatch.Draw(laser.sprite, laser.position += laser.vitesse, Color.White);
                     //}
-                }             
+                }
             }
             #endregion
             #region Dessin explosion ennemi
-            for (int s =0; s<tableauennemi.Length;s++)
+            for (int s = 0; s < tableauennemi.Length; s++)
             {
-                if(tableauennemi[s].estvivant ==false)
+                if (tableauennemi[s].estvivant == false)
                 {
                     spriteBatch.Draw(ExplosionEnnemi.sprite, ExplosionEnnemi.position += ExplosionEnnemi.vitesse, Color.White);
-                    tableauennemi[s].estvivant = true;
                     
+
                 }
             }
             #endregion
-            #region Dessin Hero + projectile hero vivant
+            #region Dessin Hero + projectile hero vivant et mort
 
             if (Hero.estvivant == true)
             {
+             
                 spriteBatch.Draw(Hero.sprite, Hero.position += Hero.vitesse, Color.White);
-                if(projectilehero.estvivant ==false)
+                for (int c = 0; c < projectilehéro.Length; c++)
                 {
-                    spriteBatch.Draw(projectilehero.sprite, projectilehero.position += projectilehero.vitesse, Color.White);
+                    if (projectilehéro[c].estvivant == false)
+                    {
+                        spriteBatch.Draw(projectilehéro[c].sprite, projectilehéro[c].position += projectilehéro[c].vitesse, Color.White);
+                        
+                    }
                 }
             }
-            #endregion
+            if(Hero.estvivant ==false)
+            {
+                if (explosionhero.estvivant ==true)
+                {
+                    spriteBatch.Draw(explosionhero.sprite, explosionhero.position += explosionhero.vitesse, Color.White);
+                }
 
+                if (explosionhero.position.Y > fenêtre.Bottom)
+                {
+                    Hero.estvivant = true;
+                    Hero.position.X = 25;
+                    Hero.position.Y = 600;
+                    compteurexplohero = 0;
+                    explosionhero.estvivant = true;
+                }
+            }
+
+            spriteBatch.DrawString(font,gameTime.TotalGameTime.Minutes.ToString()+", " + gameTime.TotalGameTime.Seconds.ToString() + ", " + gameTime.TotalGameTime.Milliseconds.ToString() + "", new Vector2(100, 100), Color.White);
+            #endregion
+        
             spriteBatch.End();
 
             // TODO: Add your drawing code here
